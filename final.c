c04a6b5ecbe9f5260495ac647d5eb1a3f25fc1d0
Safal Bolakhe <safalbolakhe@gmail.com>
2022-08-27 21:48:57 +0545

    Changed variable names and added comments to make it easier to understand

--- final.c
+++ final.c
@@ -3,192 +3,203 @@
 #include <ctype.h>
 #include <stdbool.h>
 
-// q vaneko row w vaneko column matrix ma
-int solver(int fmat[9][9], int g, int tempa, int list[g][2]);
-void backtrack(int value, int fmat[9][9], int g, int list[90][2]);
-bool checker(int fmat[9][9], int k, int l, int ans);
-int looper(int i, int j, int a, int fmat[9][9], int q, int w);
+int solver(int fmat[9][9], int driver_value, int temp_value, int empty_cell_list[driver_value][2]);
+bool checker(int fmat[9][9], int temp_row, int temp_col, int ans);
+int looper(int i, int j, int answer, int fmat[9][9], int row, int column);
 
-bool box(int a, int fmat[9][9], int q, int w);
-bool horizontal(int a, int fmat[9][9], int q);
-bool vertical(int a, int fmat[9][9], int w);
+bool box(int answer, int fmat[9][9], int row, int column);
+bool horizontal(int answer, int fmat[9][9], int row);
+bool vertical(int answer, int fmat[9][9], int column);
 
 int main(void)
 {
-    int count = 0, z = 0, g = 0;
-    int nmat[9][9], list[90][2];
+    int column = 0, row = 0, driver_value = 0;
+    int sudoku_matrix[9][9], empty_cell_list[90][2];
     char h;
     FILE *fp, *fd;
     fp = fopen("test.txt", "r");
     fd = fopen("result.txt", "w");
-    // converts file data to matrix
-    while (z != 9)
+    if (fp == NULL)
     {
-        h = fgetc(fp);
-        if (isdigit(h))
+        //this part handles if the file is not available
+        printf("Please create a file called test.txt with the sudoku to solve");
+    }
+    else
+    {
+        // converts file data to matrix
+        while (row != 9)
         {
-            nmat[z][count] = atoi(&h);
-            count++;
-            if (count == 9)
+            h = fgetc(fp);
+            if (isdigit(h))
             {
-                count = 0;
-                z++;
+                sudoku_matrix[row][column] = atoi(&h);
+                column++;
+                if (column == 9)
+                {
+                    column = 0;
+                    row++;
+                }
             }
         }
-    }
-    g = 0;
-    for (int k = 0; k < 9; k++)
-    {
-        for (int l = 0; l < 9; l++)
+        // here the driver_value keeps track of empty cells on the sudoku to solve later. It is stored in sets of two. 
+        // for example, when driver_value == 0, it stores two values in places (0,0), and (0, 1). this is the location of the empty cells
+        driver_value = 0;
+        for (int k = 0; k < 9; k++)
         {
-            if (nmat[k][l] == 0)
+            for (int l = 0; l < 9; l++)
             {
-                list[g][0] = k;
-                list[g][1] = l;
-                // printf("%i %i\n", list[g][0], list[g][1]);
-                g++;
+                if (sudoku_matrix[k][l] == 0)
+                {
+                    empty_cell_list[driver_value][0] = k;
+                    empty_cell_list[driver_value][1] = l;
+                    // printf("%i %i\n", empty_cell_list[g][0], empty_cell_list[g][1]);
+                    driver_value++;
+                }
             }
         }
-    }
 
-    int counter = g;
-    int k, l, tempa, tempval;
-    g = 0;
-    while (g < counter || g == 0)
-    {
-        k = list[g][0];
-        l = list[g][1];
-        tempa = 1;
-        nmat[k][l] = solver(nmat, g, tempa, list);
-        while (nmat[k][l] == 0)
+        // this is the main part that is responsible for solving the sudoku
+        int counter = driver_value;
+        int temp_row, temp_col, temp_value;
+        driver_value = 0;
+        while (driver_value < counter || driver_value == 0)
         {
-            g--;
-            k = list[g][0];
-            l = list[g][1];
-            tempa = nmat[k][l];
-            nmat[k][l] = solver(nmat, g, tempa, list);
+            temp_row = empty_cell_list[driver_value][0];
+            temp_col = empty_cell_list[driver_value][1];
+            temp_value = 1;
+            sudoku_matrix[temp_row][temp_col] = solver(sudoku_matrix, driver_value, temp_value, empty_cell_list);
+            while (sudoku_matrix[temp_row][temp_col] == 0)
+            {
+                driver_value--;
+                temp_row = empty_cell_list[driver_value][0];
+                temp_col = empty_cell_list[driver_value][1];
+                temp_value = sudoku_matrix[temp_row][temp_col];
+                sudoku_matrix[temp_row][temp_col] = solver(sudoku_matrix, driver_value, temp_value, empty_cell_list);
+            }
+            driver_value++;
         }
-        g++;
-    }
-    if (g == 0)
-    {
-        printf("this sudoku sucks and has no solution");
-        return 0;
-    }
+        if (driver_value == 0)
+        {
+            printf("this sudoku sucks and has no solution");
+            return 0;
+        }
 
-    for (int k = 0; k < 9; k++)
-    {
-        for (int l = 0; l < 9; l++)
+        for (int k = 0; k < 9; k++)
         {
-            fprintf(fd, "%i ", nmat[k][l]);
+            for (int l = 0; l < 9; l++)
+            {
+                fprintf(fd, "%i ", sudoku_matrix[k][l]);
+            }
+            fprintf(fd, "\n");
         }
-        fprintf(fd, "\n");
+        // system("cd /home/safalbolakhe/Desktop/C/Final Sudoku/");
+        fclose(fp);
+        fclose(fd);
+        system("kate result.txt");
     }
-    fclose(fp);
-    fclose(fd);
 }
 
 // other functions start here
 
-int solver(int fmat[9][9], int g, int tempa, int list[g][2])
+int solver(int fmat[9][9], int driver_value, int temp_value, int empty_cell_list[driver_value][2])
 {
-    int k = list[g][0];
-    int l = list[g][1];
-    while (tempa <= 9)
+    int temp_row = empty_cell_list[driver_value][0];
+    int temp_col = empty_cell_list[driver_value][1];
+    while (temp_value <= 9)
     {
         // printf("here");
-        if (checker(fmat, k, l, tempa))
+        if (checker(fmat, temp_row, temp_col, temp_value))
         {
-            return tempa;
+            return temp_value;
         }
         else
         {
-            tempa = tempa + 1;
+            temp_value = temp_value + 1;
         }
     }
     return 0;
-    // while (a <= 9)
+    // while (answer <= 9)
     // {
-    //     if (box(a, fmat, k, l) && horizontal(a, fmat, k) && vertical(a, fmat, l))
+    //     if (box(answer, fmat, temp_row, temp_col) && horizontal(answer, fmat, temp_row) && vertical(answer, fmat, temp_col))
     //     {
-    //         return a;
+    //         return answer;
     //     }
     //     else
     //     {
-    //         a++;
+    //         answer++;
     //     }
     // }
 }
-bool checker(int fmat[9][9], int k, int l, int ans)
+bool checker(int fmat[9][9], int temp_row, int temp_col, int ans)
 {
-    int a = ans;
-    if (box(a, fmat, k, l) && horizontal(a, fmat, k) && vertical(a, fmat, l))
+    int answer = ans;
+    if (box(answer, fmat, temp_row, temp_col) && horizontal(answer, fmat, temp_row) && vertical(answer, fmat, temp_col))
     {
         return true;
     }
     return false;
 }
-bool box(int a, int fmat[9][9], int q, int w)
+bool box(int answer, int fmat[9][9], int row, int column)
 {
-    if (q <= 2)
+    if (row <= 2)
     {
         // printf("0-2\n");
-        if (w <= 2)
+        if (column <= 2)
         {
-            return (looper(0, 0, a, fmat, 3, 3));
+            return (looper(0, 0, answer, fmat, 3, 3));
         }
-        else if (w >= 3 && w <= 5)
+        else if (column >= 3 && column <= 5)
         {
-            return (looper(0, 3, a, fmat, 3, 6));
+            return (looper(0, 3, answer, fmat, 3, 6));
         }
-        else if (w >= 5 && w <= 8)
+        else if (column >= 5 && column <= 8)
         {
-            return (looper(0, 6, a, fmat, 3, 9));
+            return (looper(0, 6, answer, fmat, 3, 9));
         }
     }
-    else if (3 <= q && q <= 5)
+    else if (3 <= row && row <= 5)
     {
         // printf("3-5\n");
 
-        if (w <= 2)
+        if (column <= 2)
         {
-            return (looper(3, 0, a, fmat, 6, 3));
+            return (looper(3, 0, answer, fmat, 6, 3));
         }
-        else if (w >= 2 && w <= 5)
+        else if (column >= 2 && column <= 5)
         {
-            return (looper(3, 3, a, fmat, 6, 6));
+            return (looper(3, 3, answer, fmat, 6, 6));
         }
-        else if (w >= 6 && w <= 8)
+        else if (column >= 6 && column <= 8)
         {
-            return (looper(3, 6, a, fmat, 6, 9));
+            return (looper(3, 6, answer, fmat, 6, 9));
         }
     }
-    else if (5 <= q && q <= 8)
+    else if (5 <= row && row <= 8)
     {
         // printf("6-8\n");
 
-        if (w <= 2)
+        if (column <= 2)
         {
-            return (looper(6, 0, a, fmat, 9, 3));
+            return (looper(6, 0, answer, fmat, 9, 3));
         }
-        else if (2 <= w && w <= 5)
+        else if (2 <= column && column <= 5)
         {
-            return (looper(6, 3, a, fmat, 9, 6));
+            return (looper(6, 3, answer, fmat, 9, 6));
         }
-        else if (5 <= w && w <= 8)
+        else if (5 <= column && column <= 8)
         {
-            return (looper(6, 6, a, fmat, 9, 9));
+            return (looper(6, 6, answer, fmat, 9, 9));
         }
     }
 }
-int looper(int i, int j, int a, int qmat[9][9], int q, int w)
+int looper(int i, int j, int answer, int qmat[9][9], int row, int column)
 {
-    for (int f = i; f < q; f++)
+    for (int f = i; f < row; f++)
     {
-        for (int g = j; g < w; g++)
+        for (int driver_value = j; driver_value < column; driver_value++)
         {
             int p = qmat[i][j];
-            if (qmat[f][g] == a)
+            if (qmat[f][driver_value] == answer)
             {
                 return false;
             }
@@ -197,23 +208,23 @@ int looper(int i, int j, int a, int qmat[9][9], int q, int w)
     return true;
 }
 
-bool horizontal(int a, int fmat[9][9], int q)
+bool horizontal(int answer, int fmat[9][9], int row)
 {
     for (int i = 0; i < 9; i++)
     {
-        if (fmat[q][i] == a)
+        if (fmat[row][i] == answer)
         {
-            // printf("%i h\n", fmat[q][i]);
+            // printf("%i h\n", fmat[row][i]);
             return false;
         }
     }
     return true;
 }
-bool vertical(int a, int fmat[9][9], int w)
+bool vertical(int answer, int fmat[9][9], int column)
 {
     for (int i = 0; i < 9; i++)
     {
-        if (fmat[i][w] == a)
+        if (fmat[i][column] == answer)
         {
             return false;
         }
